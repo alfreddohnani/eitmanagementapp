815fccf2a8a90c2c31ca5c2120a333be2313a576
Alfred Doh-Nani <alfreddohnani@pop-os.localdomain>
2019-10-18 14:17:17 +0000

    Add custom.scss and bootstrap.css to App.jsx

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { LoginPage } from './pages/Login/LoginPage.component';
 
+import '../ui/scss/custom.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+
 const App = () => (
   <div>
     <LoginPage />
