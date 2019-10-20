b49e0f9873767fcd8ae5637af331f8ed549a3d88
Alfred Doh-Nani <alfreddohnani@pop-os.localdomain>
2019-10-20 13:51:00 +0000

    Replace App component with renderRoutes() to be able to use react router during startup

--- client/main.jsx
+++ client/main.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-import App from '/imports/ui/App'
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { render } from "react-dom";
+import App from "/imports/ui/App";
+import { renderROutes, renderRoutes } from "../imports/startup/client/routes";
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('react-target'));
+  render(renderRoutes(), document.getElementById("react-target"));
 });
