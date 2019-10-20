ce792d85c425e0a864cea5b493d87b9ca1ea27f3
Alfred Doh-Nani <alfreddohnani@pop-os.localdomain>
2019-10-20 17:28:36 +0000

    Delete all ui subfolders to start all over again because of the numerous errors in routing

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router";
 
-import createBrowserHistory from "history/createBrowserHistory";
+import { createBrowserHistory } from "history";
 
 // Route components
 import App from "../../ui/App";
-
+import EitList from "../../ui/components/EitList/EitList.component";
 const browserHistory = createBrowserHistory();
 
 export const renderRoutes = () => (
   <Router history={browserHistory}>
     <Switch>
       <Route exact path="/" component={App} />{" "}
+      <Route exact path="/eits" component={EitList} />
     </Switch>{" "}
   </Router>
 );
